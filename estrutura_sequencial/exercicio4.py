1786dfb0b4b5b033b89d924cb034ed6940deb97b
Enzzo D.N <enzzodiasnogueira2@gmail.com>
2023-09-12 17:57:32 -0300

    Create exercicio4.py

---
+++ estrutura_sequencial/exercicio4.py
@@ -0,0 +1,22 @@
+#Q.4 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+import os 
+os.system('cls')
+
+print("--------------------------------")
+print("-------- MEDIA BIMESTRAL -------")
+print("--------------------------------")
+
+print("Digite suas 4 notas bimestrais e sera dita sua media e seu resultado final: \n")
+n1=input("N1: ")
+n2=input("N2: ")
+n3=input("N3: ")
+n4=input("N4: ")
+print("---------------")
+
+m=(float(n1)+float(n2)+float(n3)+float(n4))/4
+
+print("Essa foi sua media: "+str(m))
+
+"""if (m<=7):
+    print("sua ")"""
