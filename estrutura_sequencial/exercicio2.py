61a9ed87cc05c9574d97e788ac62bf822ad15b18
Enzzo D.N <enzzodiasnogueira2@gmail.com>
2023-09-12 17:26:25 -0300

    Update exercicio2.py

--- estrutura_sequencial/exercicio2.py
+++ estrutura_sequencial/exercicio2.py
@@ -1,2 +1,7 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+import os
+os.system('cls')
+
 num=input("digite um numero: ")
 print("o numero digitado foi: "+num)
