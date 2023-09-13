85f3c3b0f13611d72c186e95412dd729dad0b910
Enzzo D.N <enzzodiasnogueira2@gmail.com>
2023-09-13 17:24:17 -0300

    Update exercicio6.py

--- estrutura_de_decisao/exercicio6.py
+++ estrutura_de_decisao/exercicio6.py
@@ -21,9 +21,9 @@ print("N3: "+str(n3))
 
 print("---------------- \n")
 
-if n1>n2 and n1>n3:
+if n1>=n2 and n1>=n3:
     print("O maior eh: "+str(n1))
-elif n2>n1 and n2>n3:
+elif n2>=n1 and n2>=n3:
     print("O maior eh: "+str(n2))
-elif n3>n1 and n3>n2:
+elif n3>=n1 and n3>=n2:
     print("O maior eh: "+str(n3))
