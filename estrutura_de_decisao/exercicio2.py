9e6e20e59631c8068a5241ee5cdd173267b5d9b0
Enzzo D.N <enzzodiasnogueira2@gmail.com>
2023-09-13 15:58:37 -0300

    Create exercicio2.py

---
+++ estrutura_de_decisao/exercicio2.py
@@ -0,0 +1,18 @@
+#Q.2 Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+import os 
+
+
+os.system('cls')
+
+print("--------------------------------")
+print("--------- PAR OU IMPAR ---------")
+print("--------------------------------")
+
+num=float(input("Digite um numero e verifique se ele eh par ou impar: \n"))
+print("-----------------")
+
+if num%2 == 0:
+    print("Seu numero eh PAR")
+else:
+    print("Seu numero eh IMPAR")
