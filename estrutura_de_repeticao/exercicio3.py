000b4cfe1a2acf126e6bccf950aa08d111698923
Enzzo D.N <enzzodiasnogueira2@gmail.com>
2023-09-26 15:00:21 -0300

    Update exercicio3.py

--- estrutura_de_repeticao/exercicio3.py
+++ estrutura_de_repeticao/exercicio3.py
@@ -12,89 +12,131 @@ print("----------------------------------------")
 print("------------ DADOS PESSOAIS ------------")
 print("---------------------------------------- \n")
 
-print("Digite abaixo os dados que se pedem: \n")
+print("Digite abaixo os dados que se pedem: ")
+
+# ------ PARTE 1 (NOME) -----------
 nome=input("NOME: ")
-idd=int(input("IDADE: "))
-sal=float(input("SALARIO: "))
-sexo=input("SEXO ('m' ou 'f'): ")
-ec=input("ESTADO CIVIL ('s', 'c', 'v' ou 'd'): ")
 
-# ------ NOME ------
-quantnome = len(nome)
+quantnome= len(nome)
 while (quantnome < 3):
-    print("---------------------------- \n")
-    print(" ------- ATENCAO ------- ")
-    print("Seu nome deve ter mais de 3 caracteres!")
-    print("Por favor, digite novamente.")
-    teste=nome
-    break
+    if True:
+        print("Seu nome deve ter mais de 3 caracteres!")
+        print("Por favor, digite novamente.")
+    nome=input("NOME: ")
+    quantnome= len(nome)
 
-# ------ IDADE ------
+os.system('cls')
+print("----------------------------------------")
+print("------------ DADOS PESSOAIS ------------")
+print("---------------------------------------- \n")
+print("Digite abaixo os dados que se pedem: ")
+
+print("NOME: " + nome)
+
+# ------ PARTE 2 (IDADE) -----------
+idd=int(input("IDADE: "))
+
 while (idd < 0) or (idd > 150):
-    print("---------------------------- \n")
-    print(" ------- ATENCAO ------- ")
     print("Sua idade deve ser entre 0 e 150 anos!")
     print("Por favor, digite novamente.")
-    teste=idd
-    break
+    idd=int(input("IDADE: "))
+    
+os.system('cls')
+print("----------------------------------------")
+print("------------ DADOS PESSOAIS ------------")
+print("---------------------------------------- \n")
+print("Digite abaixo os dados que se pedem: ")
 
-# ------ SALARIO ------
+print("NOME: " + nome)
+print("IDADE: "+ str(idd))
+
+# ------ PARTE 3 (SALÁRIO) -----------
+
+sal=float(input("SALARIO: "))
+
 while (sal <= 0):
-    print("---------------------------- \n")
-    print(" ------- ATENCAO ------- ")
     print("Seu salario deve ser maior que 0!")
     print("Por favor, digite novamente.")
-    teste=sal
-    break
+    sal=float(input("SALARIO: "))
 
-# ------ SEXO ------
-while (sexo != "m") and (sexo != "f"):
-    print("---------------------------- \n")
-    print(" ------- ATENCAO ------- ")
-    print("Seu sexo deve ser definido somente em 'm' ou 'f'!")
+os.system('cls')
+print("----------------------------------------")
+print("------------ DADOS PESSOAIS ------------")
+print("---------------------------------------- \n")
+print("Digite abaixo os dados que se pedem: ")
+
+print("NOME: " + nome)
+print("IDADE: "+ str(idd))
+print("SALARIO: R$ "+ str(sal))
+
+# ------ PARTE 4 (SEXO) -----------
+
+sexo=input("SEXO ('m' ou 'f'): ")
+
+if (sexo == "m") or (sexo == "M"):
+    sexo = "Masculino"
+elif (sexo == "f") or (sexo == "F"):
+    sexo = "Feminino"
+
+while (sexo != "Masculino") and (sexo != "Feminino"):
+    print("Seu sexo deve ser definido somente com 'm' ou 'f'!")
     print("Por favor, digite novamente.")
-    teste=sexo
-    break
+    sexo=input("SEXO ('m' ou 'f'): ")
+    if (sexo == "m") or (sexo == "M"):
+        sexo = "Masculino"
+    elif (sexo == "f") or (sexo == "F"):
+        sexo = "Feminino"
 
-# ------ ESTADO CIVIL ------
-while (ec != "s") and (ec != "c") and (ec != "v") and (ec != "d"):
-    print("---------------------------- \n")
-    print(" ------- ATENCAO ------- ")
+os.system('cls')
+print("----------------------------------------")
+print("------------ DADOS PESSOAIS ------------")
+print("---------------------------------------- \n")
+print("Digite abaixo os dados que se pedem: ")
+
+print("NOME: " + nome)
+print("IDADE: "+ str(idd))
+print("SALARIO: R$ "+ str(sal))
+print("SEXO: "+ sexo)
+
+# ------ PARTE 5 (ESTADO CIVIL) -----------
+
+ec=input("ESTADO CIVIL ('s', 'c', 'v' ou 'd'): ")
+
+if (ec == "s") or (ec == "S"):
+    ec = "Solteiro(a)"
+elif (ec == "c") or (ec == "C"):
+    ec = "Casado(a)"
+elif (ec == "v") or (ec == "V"):
+    ec = "Viuvo(a)"
+elif (ec == "d") or (ec == "D"):
+    ec = "Divorciado(a)"
+
+while (ec != "Solteiro(a)") and (ec != "Casado(a)") and (ec != "Viuvo(a)") and (ec != "Divorciado(a)"):
     print("Seu estado civil deve ser: 's', 'c', 'v' ou 'd'!")
     print("Por favor, digite novamente.")
-    teste=ec
-    break   
+    ec=input("ESTADO CIVIL ('s', 'c', 'v' ou 'd'): ")
+    if (ec == "s") or (ec == "S"):
+        ec = "Solteiro(a)"
+    elif (ec == "c") or (ec == "C"):
+        ec = "Casado(a)"
+    elif (ec == "v") or (ec == "V"):
+        ec = "Viuvo(a)"
+    elif (ec == "d") or (ec == "D"):
+        ec = "Divorciado(a)"
 
-# FUNCIONALIDADE
+os.system('cls')
+print("----------------------------------------")
+print("------------ DADOS PESSOAIS ------------")
+print("---------------------------------------- \n")
+print("Digite abaixo os dados que se pedem: ")
 
+print("NOME: " + nome)
+print("IDADE: "+ str(idd))
+print("SALARIO: R$ "+ str(sal))
+print("SEXO: "+ sexo)
+print("ESTADO CIVIL: "+ ec)
 
-
-teste=nome
-teste=idd 
-teste=sal 
-teste=sexo 
-teste=ec
-
-# PRA DAR ERRADO
-
-
-
-# PRA DAR CERTO
-
-"""if (teste == nome) or (teste == idd) or (teste == sal) or (teste == sexo) or (teste == ec):
-    print("   ")
-    exit()
-elif (teste != nome) or (teste != idd) or (teste != sal) or (teste != sexo) or (teste != ec):
-    print("   ")
-    exit()
-
-if (teste == nome) and (teste == idd) and (teste == sal) and (teste == sexo) and (teste == ec):
-    print("---------------------------- \n")
-    print("Seus dados pessoais atendem todos os nossos requisitos!")
-    print("Aguarde e voce sera redirecionado para outra pagina.")
-    print(" ")
-elif (teste != nome) and (teste != idd) and (teste != sal) and (teste != sexo) and (teste != ec):
-    print("---------------------------- \n")
-    print("Seus dados pessoais atendem todos os nossos requisitos!")
-    print("Aguarde e voce sera redirecionado para outra pagina.")
-    print(" ")"""
+print("------------------------------- \n")
+print("Seus dados pessoais atendem todos os nosso requisitos!")
+print("Auarde um momento e voce ja sera direcionado para outra pagina.")
+print(" ")
