1808837a06a845ca9cb567d267776b3651d0118f
Enzzo D.N <enzzodiasnogueira2@gmail.com>
2023-09-27 16:01:37 -0300

    Create exercicio15.py

---
+++ estrutura_de_repeticao/exercicio15.py
@@ -0,0 +1,19 @@
+"""A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,...
+Faça um programa capaz de gerar a série até o n−ésimo termo."""
+
+import os 
+os.system('cls')
+print("-----------------------------------------")
+print("--------------- FIBONACCI ---------------")
+print("----------------------------------------- \n")
+
+print("A serie de Fibonacci é formada pela sequencia 0,1,1,2,3,5,8,13,21,34,55,...\n")
+print("----------------------------------\n")
+
+pos=int(input("POSICAO: "))
+print("\n----------------------------------\n")
+n1=1
+num=0
+while (num <= pos):
+    print(num)
+    num=n1+num
